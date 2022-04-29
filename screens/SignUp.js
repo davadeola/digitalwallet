68da0f37d1b6851892e79b5991426c87540d5870
davadeola <adeoladavid15@gmail.com>
2022-04-29 12:25:11 +0300

    added sign up password view toggle

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -16,119 +16,71 @@ import {COLORS, SIZES, icons, images, FONTS} from '../constants';
 
 import LinearGradient from 'react-native-linear-gradient';
 
-//renderHeader Functional Component
-const renderHeader = () => {
-  return (
-    <TouchableOpacity
-      style={{
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginTop: SIZES.padding * 6,
-        paddingHorizontal: SIZES.padding * 2,
-      }}
-      onPress={() => console.log('pressed')}>
-      <Image
-        source={icons.back}
-        resizeMode="contain"
-        style={{width: 20, height: 20, tintColor: COLORS.white}}
-      />
-      <Text
+const SignUp = () => {
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  //renderHeader Functional Component
+  const renderHeader = () => {
+    return (
+      <TouchableOpacity
         style={{
-          marginLeft: SIZES.padding * 1.5,
-          color: COLORS.white,
-          ...FONTS.h4,
-        }}>
-        Sign Up
-      </Text>
-    </TouchableOpacity>
-  );
-};
-
-//renderLogo Functional Component
-const renderLogo = () => {
-  return (
-    <View
-      style={{
-        marginTop: SIZES.padding * 5,
-        height: 100,
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}>
-      <Image
-        source={images.wallieLogo}
-        resizeMode="contain"
-        style={{width: '60%'}}
-      />
-    </View>
-  );
-};
-
-//renderForm Functional Component
-const renderForm = () => {
-  return (
-    <View
-      style={{
-        marginTop: SIZES.padding * 3,
-        marginHorizontal: SIZES.padding * 3,
-      }}>
-      {/* Full Name */}
-      <View style={{marginTop: SIZES.padding * 3}}>
-        <Text style={{color: COLORS.lightGreen, ...FONTS.body3}}>
-          Full Name
-        </Text>
-        <TextInput
+          flexDirection: 'row',
+          alignItems: 'center',
+          marginTop: SIZES.padding * 6,
+          paddingHorizontal: SIZES.padding * 2,
+        }}
+        onPress={() => console.log('pressed')}>
+        <Image
+          source={icons.back}
+          resizeMode="contain"
+          style={{width: 20, height: 20, tintColor: COLORS.white}}
+        />
+        <Text
           style={{
-            marginVertical: SIZES.padding,
-            borderBottomColor: COLORS.white,
-            borderBottomWidth: 1,
-            height: 40,
+            marginLeft: SIZES.padding * 1.5,
             color: COLORS.white,
-            ...FONTS.body3,
-          }}
-          placeholder="Enter Full Name"
-          placeholderTextColor={COLORS.white}
-          selectionColor={COLORS.white}
+            ...FONTS.h4,
+          }}>
+          Sign Up
+        </Text>
+      </TouchableOpacity>
+    );
+  };
+
+  //renderLogo Functional Component
+  const renderLogo = () => {
+    return (
+      <View
+        style={{
+          marginTop: SIZES.padding * 5,
+          height: 100,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <Image
+          source={images.wallieLogo}
+          resizeMode="contain"
+          style={{width: '60%'}}
         />
       </View>
+    );
+  };
 
-      {/* Phone Number */}
-      <View style={{marginTop: SIZES.padding * 2}}>
-        <Text style={{color: COLORS.lightGreen, ...FONTS.body3}}>
-          Phone Number
-        </Text>
-        <View style={{flexDirection: 'row'}}>
-          {/* Country Code */}
-          <TouchableOpacity
-            style={{
-              width: 100,
-              height: 50,
-              marginHorizontal: 5,
-              borderBottomWidth: 1,
-              borderBottomColor: COLORS.white,
-              flexDirection: 'row',
-              ...FONTS.body2,
-            }}>
-            <View style={{justifyContent: 'center'}}>
-              <Image
-                source={icons.down}
-                style={{height: 10, width: 10, tintColor: COLORS.white}}
-              />
-            </View>
-            <View style={{justifyContent: 'center', marginLeft: 5}}>
-              <Image
-                source={images.usFlag}
-                resizeMode="contain"
-                style={{height: 30, width: 30}}
-              />
-            </View>
-            <View style={{justifyContent: 'center', marginLeft: 5}}>
-              <Text style={{color: COLORS.white, ...FONTS.body3}}>US+1</Text>
-            </View>
-          </TouchableOpacity>
-
+  //renderForm Functional Component
+  const renderForm = () => {
+    return (
+      <View
+        style={{
+          marginTop: SIZES.padding * 3,
+          marginHorizontal: SIZES.padding * 3,
+        }}>
+        {/* Full Name */}
+        <View style={{marginTop: SIZES.padding * 3}}>
+          <Text style={{color: COLORS.lightGreen, ...FONTS.body3}}>
+            Full Name
+          </Text>
           <TextInput
             style={{
-              flex: 1,
               marginVertical: SIZES.padding,
               borderBottomColor: COLORS.white,
               borderBottomWidth: 1,
@@ -136,67 +88,120 @@ const renderForm = () => {
               color: COLORS.white,
               ...FONTS.body3,
             }}
-            placeholder="Enter Phone Number"
+            placeholder="Enter Full Name"
             placeholderTextColor={COLORS.white}
             selectionColor={COLORS.white}
           />
         </View>
-      </View>
 
-      {/* Password */}
-      <View style={{marginTop: SIZES.padding * 2}}>
-        <Text style={{color: COLORS.lightGreen, ...FONTS.body3}}>Password</Text>
-        <TextInput
-          style={{
-            marginVertical: SIZES.padding,
-            borderBottomColor: COLORS.white,
-            borderBottomWidth: 1,
-            height: 40,
-            color: COLORS.white,
-            ...FONTS.body3,
-          }}
-          placeholder="Enter Password"
-          placeholderTextColor={COLORS.white}
-          selectionColor={COLORS.white}
-          secureTextEntry={true}
-        />
+        {/* Phone Number */}
+        <View style={{marginTop: SIZES.padding * 2}}>
+          <Text style={{color: COLORS.lightGreen, ...FONTS.body3}}>
+            Phone Number
+          </Text>
+          <View style={{flexDirection: 'row'}}>
+            {/* Country Code */}
+            <TouchableOpacity
+              style={{
+                width: 100,
+                height: 50,
+                marginHorizontal: 5,
+                borderBottomWidth: 1,
+                borderBottomColor: COLORS.white,
+                flexDirection: 'row',
+                ...FONTS.body2,
+              }}>
+              <View style={{justifyContent: 'center'}}>
+                <Image
+                  source={icons.down}
+                  style={{height: 10, width: 10, tintColor: COLORS.white}}
+                />
+              </View>
+              <View style={{justifyContent: 'center', marginLeft: 5}}>
+                <Image
+                  source={images.usFlag}
+                  resizeMode="contain"
+                  style={{height: 30, width: 30}}
+                />
+              </View>
+              <View style={{justifyContent: 'center', marginLeft: 5}}>
+                <Text style={{color: COLORS.white, ...FONTS.body3}}>US+1</Text>
+              </View>
+            </TouchableOpacity>
+
+            <TextInput
+              style={{
+                flex: 1,
+                marginVertical: SIZES.padding,
+                borderBottomColor: COLORS.white,
+                borderBottomWidth: 1,
+                height: 40,
+                color: COLORS.white,
+                ...FONTS.body3,
+              }}
+              placeholder="Enter Phone Number"
+              placeholderTextColor={COLORS.white}
+              selectionColor={COLORS.white}
+            />
+          </View>
+        </View>
+
+        {/* Password */}
+        <View style={{marginTop: SIZES.padding * 2}}>
+          <Text style={{color: COLORS.lightGreen, ...FONTS.body3}}>
+            Password
+          </Text>
+          <TextInput
+            style={{
+              marginVertical: SIZES.padding,
+              borderBottomColor: COLORS.white,
+              borderBottomWidth: 1,
+              height: 40,
+              color: COLORS.white,
+              ...FONTS.body3,
+            }}
+            placeholder="Enter Password"
+            placeholderTextColor={COLORS.white}
+            selectionColor={COLORS.white}
+            secureTextEntry={!showPassword}
+          />
+          <TouchableOpacity
+            style={{
+              position: 'absolute',
+              right: 0,
+              bottom: 10,
+              height: 30,
+              width: 30,
+            }}
+            onPress={() => setShowPassword(!showPassword)}>
+            <Image
+              source={showPassword ? icons.disable_eye : icons.eye}
+              style={{height: 20, width: 20, tintColor: COLORS.white}}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  };
+
+  //renderButtonFunctionalComponent
+  const renderButton = () => {
+    return (
+      <View style={{margin: SIZES.padding * 3}}>
         <TouchableOpacity
           style={{
-            position: 'absolute',
-            right: 0,
-            bottom: 10,
-            height: 30,
-            width: 30,
+            height: 60,
+            backgroundColor: COLORS.black,
+            borderRadius: SIZES.radius / 1.5,
+            alignItems: 'center',
+            justifyContent: 'center',
           }}>
-          <Image
-            source={icons.eye}
-            style={{height: 20, width: 20, tintColor: COLORS.white}}
-          />
+          <Text style={{color: COLORS.white, ...FONTS.h3}}>Continue</Text>
         </TouchableOpacity>
       </View>
-    </View>
-  );
-};
+    );
+  };
 
-//renderButtonFunctionalComponent
-const renderButton = () => {
-  return (
-    <View style={{margin: SIZES.padding * 3}}>
-      <TouchableOpacity
-        style={{
-          height: 60,
-          backgroundColor: COLORS.black,
-          borderRadius: SIZES.radius / 1.5,
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}>
-        <Text style={{color: COLORS.white, ...FONTS.h3}}>Continue</Text>
-      </TouchableOpacity>
-    </View>
-  );
-};
-
-const SignUp = () => {
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS == 'ios' ? 'padding' : null}
