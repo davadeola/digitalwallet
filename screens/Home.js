3d3c7367c9617a768bcf9c626ac8ed9de98acce0
davadeola <adeoladavid15@gmail.com>
2022-05-03 09:42:04 +0300

    created the Home Screen

--- screens/Home.js
+++ screens/Home.js
@@ -100,7 +100,141 @@ const Home = () => {
   const [features, setFeatures] = React.useState(featuresData);
   const [specialPromos, setSpecialPromos] = React.useState(specialPromoData);
 
-  function renderPromos() {
+  const renderHeader = () => {
+    return (
+      <View style={{flexDirection: 'row', marginVertical: SIZES.padding * 2}}>
+        <View style={{flex: 1}}>
+          <Text style={{...FONTS.h1}}>Hello!</Text>
+          <Text style={{...FONTS.body2, color: COLORS.gray}}>David Adeola</Text>
+        </View>
+
+        <View style={{alignItems: 'center', justifyContent: 'center'}}>
+          <TouchableOpacity
+            style={{
+              height: 40,
+              width: 40,
+              justifyContent: 'center',
+              alignItems: 'center',
+              background: COLORS.lightGray,
+            }}>
+            <Image
+              source={icons.bell}
+              style={{width: 20, height: 20, tintColor: COLORS.secondary}}
+            />
+            <View
+              style={{
+                position: 'absolute',
+                top: -5,
+                right: -5,
+                height: 10,
+                width: 10,
+                backgroundColor: COLORS.red,
+                borderRadius: 5,
+              }}></View>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  };
+
+  const renderBanner = () => (
+    <View
+      style={{
+        height: 120,
+        borderRadius: 20,
+      }}>
+      <Image
+        source={images.banner}
+        resizeMode="cover"
+        style={{
+          width: '100%',
+          height: '100%',
+          borderRadius: 20,
+        }}
+      />
+    </View>
+  );
+
+  const renderFeatures = () => {
+    const Header = () => (
+      <View style={{marginBottom: SIZES.padding * 2}}>
+        <Text style={{...FONTS.h3}}>Features</Text>
+      </View>
+    );
+
+    const renderItem = ({item}) => (
+      <TouchableOpacity
+        style={{
+          marginBottom: SIZES.padding * 2,
+          width: 60,
+          alignItems: 'center',
+        }}
+        onPress={() => console.log(item.description)}>
+        <View
+          style={{
+            height: 50,
+            width: 50,
+            marginBottom: 5,
+            backgroundColor: item.backgroundColor,
+            borderRadius: 20,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <Image
+            source={item.icon}
+            resizeMode="contain"
+            style={{
+              height: 20,
+              width: 20,
+              marginBottom: 5,
+              tintColor: item.color,
+            }}
+          />
+        </View>
+        <Text style={{textAlign: 'center', flexWrap: 'wrap', ...FONTS.body4}}>
+          {item.description}
+        </Text>
+      </TouchableOpacity>
+    );
+
+    return (
+      <FlatList
+        ListHeaderComponent={Header}
+        data={features}
+        numColumns={4}
+        columnWrapperStyle={{justifyContent: 'space-between'}}
+        keyExtractor={item => `${item.id}`}
+        renderItem={renderItem}
+        style={{marginTop: SIZES.padding * 2}}
+      />
+    );
+  };
+
+  const renderPromos = () => {
+    const HeaderComponent = () => (
+      <View>
+        {renderHeader()}
+        {renderBanner()}
+        {renderFeatures()}
+        {renderPromoHeader()}
+      </View>
+    );
+
+    const renderPromoHeader = () => (
+      <View
+        style={{
+          flexDirection: 'row',
+          marginBottom: SIZES.padding,
+        }}>
+        <View style={{flex: 1}}>
+          <Text style={{...FONTS.h3}}>Special Promos</Text>
+        </View>
+        <TouchableOpacity onPress={() => console.log('View All')}>
+          <Text style={{color: COLORS.gray, ...FONTS.body4}}>View All</Text>
+        </TouchableOpacity>
+      </View>
+    );
+
     const renderItem = ({item}) => (
       <TouchableOpacity
         style={{
@@ -142,6 +276,7 @@ const Home = () => {
 
     return (
       <FlatList
+        ListHeaderComponent={HeaderComponent}
         contentContainerStyle={{paddingHorizontal: SIZES.padding * 3}}
         numColumns={2}
         columnWrapperStyle={{justifyContent: 'space-between'}}
@@ -149,9 +284,10 @@ const Home = () => {
         keyExtractor={item => `${item.id}`}
         renderItem={renderItem}
         showsVerticalScrollIndicator={false}
+        ListFooterComponent={<View style={{marginBottom: 80}}></View>}
       />
     );
-  }
+  };
 
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: COLORS.white}}>
