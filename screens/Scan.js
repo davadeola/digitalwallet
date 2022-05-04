c8dc2d49e55f8a46238b0fd6a189a1b4a0a39603
davadeola <adeoladavid15@gmail.com>
2022-05-04 19:35:34 +0300

    set up camera but it has an internal bug from ios

--- screens/Scan.js
+++ screens/Scan.js
@@ -1,10 +1,32 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image, TouchableOpacity, StyleSheet} from 'react-native';
+import {Camera, useCameraDevices} from 'react-native-vision-camera';
+
+import {COLORS, SIZES, FONTS, icons, images} from '../constants';
 
 const Scan = () => {
+  const devices = useCameraDevices();
+  const device = devices.back;
+
+  const [hasPermission, setHasPermission] = React.useState(false);
+
+  React.useEffect(() => {
+    (async () => {
+      const status = await Camera.requestCameraPermission().catch(error =>
+        console.log(error),
+      );
+      setHasPermission(status === 'authorized');
+    })();
+  }, []);
+
   return (
-    <View>
-      <Text>Scan</Text>
+    <View style={{flex: 1, backgroundColor: 'transparent'}}>
+      {device != null && hasPermission && (
+        <Camera
+          style={StyleSheet.absoluteFill}
+          isActive={true}
+          device={device}></Camera>
+      )}
     </View>
   );
 };
